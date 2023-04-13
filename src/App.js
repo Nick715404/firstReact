5e403a9953791de48b9d464329527840413ebea7
nicklllson <nicklllson.dev@gmail.com>
2023-04-13 09:04:16 +0500

    Добавил верстку

--- src/App.js
+++ src/App.js
@@ -1,30 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import Fcomponent from './Fcomponent';
-import Ccomponent from './Ccomponent';
-import Footer from './Footer'
+import "./styles/main.css";
+import sun from './images/icons/sun.svg';
+import moon from './images/icons/moon.svg';
+import vk from './images/icons/vk.svg';
+import inst from './images/icons/instagram.svg';
+import twitter from './images/icons/twitter.svg';
+import gitHub from './images/icons/gitHub.svg';
+import linkedIn from './images/icons/linkedIn.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Ccomponent name = 'Bogdan' />
+      <nav className="nav">
+        <div className="container">
+            <div className="nav-row">
+                <a href="./index.html" className="logo"><strong>Freelancer</strong> portfolio</a>
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Web developer blog
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Footer />
-      </header>
-      
+                <button className="dark-mode-btn">
+                    <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
+                    <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+                </button>
+
+                <ul className="nav-list">
+                    <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
+                    <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
+                    <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header className="header">
+        <div className="header__wrapper">
+            <h1 className="header__title">
+                <strong>Hi, my name is <em>Yuri</em></strong><br />
+                a frontend developer
+            </h1>
+            <div className="header__text">
+                <p>with passion for learning and creating.</p>
+            </div>
+            <a href="#!" className="btn">Download CV</a>
+        </div>
+    </header>
+
+    <main className="section">
+        <div className="container">
+
+                <ul className="content-list">
+                    <li className="content-list__item">
+                        <h2 className="title-2">Frontend</h2>
+                        <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Backend</h2>
+                        <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+                    </li>
+                </ul>
+
+        </div>
+    </main>
+
+    <footer className="footer">
+        <div className="container">
+            <div className="footer__wrapper">
+                <ul className="social">
+                    <li className="social__item"><a href="#!"><img src={vk} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={inst} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={gitHub} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
+                </ul>
+                <div className="copyright">
+                    <p>© 2022 frontend-dev.com</p>
+                </div>
+            </div>
+        </div>
+    </footer>
     </div>
   );
 }
